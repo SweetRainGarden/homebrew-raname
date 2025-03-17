8d91c4fffd895a8bde1422362010402eb783daf1
Randy <rtang@wyze.com>
2025-03-16 18:57:25 -0700

    Update Brew formula URL and SHA256 hash for version 1.0.0.0

--- Formula/rename.rb
+++ Formula/rename.rb
@@ -1,8 +1,8 @@
 class Rename < Formula
   desc "Recursively rename files, directories, and replace content"
   homepage "https://github.com/SweetRainGarden/BrewRename"
-  url "https://github.com/SweetRainGarden/BrewRename/archive/refs/tags/v1.0.tar.gz"
-  sha256 "YOUR_TARBALL_SHA256_HASH"
+  url "https://github.com/SweetRainGarden/BrewRename/archive/refs/tags/1.0.0.0.zip"
+  sha256 "a2777bddd0a986345bc74de4c018606da38fab56"
   version "1.0"
   license "MIT"
 
