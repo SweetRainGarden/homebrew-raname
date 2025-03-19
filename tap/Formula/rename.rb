70e19a083d0669a2564b55a37602aba99145c1e3
Randy <rtang@wyze.com>
2025-03-18 21:44:10 -0700

    Update formula for v1.1.0.0 release

--- tap/Formula/rename.rb
+++ tap/Formula/rename.rb
@@ -1,8 +1,8 @@
 class Rename < Formula
   desc "A utility to rename files and directories, replacing text in both names and content"
   homepage "https://github.com/SweetRainGarden/homebrew-rename"
-  url "https://github.com/SweetRainGarden/homebrew-rename/archive/refs/heads/develop.zip"
-  version "1.0.1.0"
+  url "https://github.com/SweetRainGarden/homebrew-rename/archive/refs/tags/v1.1.0.0.tar.gz"
+  version "1.1.0.0"
 
   def install
     bin.install "bin/rename.sh" => "rename"
